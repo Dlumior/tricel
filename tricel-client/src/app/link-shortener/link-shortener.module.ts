5213bdeab1da0fe1505a100208b8808bfc5dd6e2
dlumior <ld_ramirez27@outlook.com>
2024-04-11 19:32:43 -0500

    feat: add the link shortener home-page

--- tricel-client/src/app/link-shortener/link-shortener.module.ts
+++ tricel-client/src/app/link-shortener/link-shortener.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { LinkCardComponent } from './components/links/link-card/link-card.component';
 import { LinkListComponent } from './components/links/link-list/link-list.component';
 import { StatsInfoComponent } from './components/stats-info/stats-info.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [LinkCardComponent, LinkListComponent, StatsInfoComponent],
-  exports: [LinkListComponent, StatsInfoComponent],
-  imports: [CommonModule],
+  declarations: [
+    LinkCardComponent,
+    LinkListComponent,
+    StatsInfoComponent,
+    HomePageComponent,
+  ],
+  exports: [HomePageComponent],
+  imports: [CommonModule, SharedModule],
 })
 export class LinkShortenerModule {}
---
+++ tricel-client/src/app/link-shortener/pages/home-page/home-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'link-shortener-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css',
+})
+export class HomePageComponent {}
